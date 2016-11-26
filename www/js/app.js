7cd22a20ae6d958d9da39dcbae7fc79f9eafeaeb
t1t1 <cha@hanmail2.net>
2016-11-26 13:45:32 +0900

    add list page

--- www/js/app.js
+++ www/js/app.js
@@ -3,7 +3,28 @@
 // angular.module is a global place for creating, registering and retrieving Angular modules
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
-angular.module('starter', ['ionic'])
+angular.module('mytodos', ['ionic'])
+
+.controller('ListCtrl', function($scope) {
+  $scope.todos = [
+    {
+      title: 'title',
+      description: 'description',
+      complete: false
+    },
+    {
+      title: 'title',
+      description: 'description',
+      complete: false
+    },
+    {
+      title: 'title',
+      description: 'description',
+      complete: false
+    }
+
+  ];
+})
 
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
